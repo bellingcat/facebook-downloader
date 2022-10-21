864f80921e9532e889c4a884e44c7b9bf27adfe8
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-10-21 23:23:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 
 setuptools.setup(
     name='facebook-downloader',
-    version='2022.1.2.0',
+    version='2022.1.2.1',
     author='Richard Mwewa',
     author_email='rly0nheart@duck.com',
     packages=['facebook_downloader'],
