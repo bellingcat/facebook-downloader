3ae6d79d1a6ec15de16bb5f5a5d4896dfedf18aa
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-10-12 15:18:46 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@
 # Remove the geckodriver binary from /usr/bin
 sudo rm /usr/bin/geckodriver -v
 
-# Uninstall tor2tor
+# Uninstall facebook-downloader
 pip3 uninstall facebook-downloader -y -v
 echo "Cleanup complete."
